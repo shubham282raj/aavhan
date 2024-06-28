8fb148a0aaf58f28c9bbb26099b0c910320da161
Shubham Raj <shubham282raj@gmail.com>
2024-06-28 10:53:09 +0530

    Frontend: Added Responsive Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  const isLoggedIn = true;
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const navigationLinks = isLoggedIn
+    ? [
+        ["/", "Home"],
+        ["/profile", "Profile"],
+      ]
+    : [
+        ["/", "Home"],
+        ["/join-us", "Join Us!"],
+        ["/login", "Login"],
+      ];
+
+  return (
+    <div className="bg-gray-200 py-3 flex flex-col sm:flex-row justify-between items-center">
+      <div className="flex justify-between w-full sm:w-auto">
+        <span className="text-3xl font-bold tracking-tight">
+          <Link to="/">Food Booking</Link>
+        </span>
+        <div
+          className="sm:hidden text-3xl cursor-pointer"
+          onClick={() => setToggleMenu(!toggleMenu)}
+        >
+          &#9776;
+        </div>
+      </div>
+      <div>
+        <div
+          className={`flex flex-col justify-center items-center sm:flex sm:flex-row text-xl gap-5 ${
+            toggleMenu ? "flex" : "hidden"
+          }`}
+        >
+          {navigationLinks.map((link, index) => {
+            return (
+              <Link
+                to={link[0]}
+                key={index}
+                onClick={() => setToggleMenu(false)}
+              >
+                {link[1]}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
