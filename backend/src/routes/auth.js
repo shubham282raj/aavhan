697eb84bb026e7d7c14282f1cd8b55251c232dbf
Shubham Raj <shubham282raj@gmail.com>
2024-07-01 19:16:25 +0530

    Deployment: Fixed sameSite cookies error in sign-out route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -73,6 +73,9 @@ auth.get("/validate-token", verifyToken, (req, res) => {
 
 auth.post("/logout", (req, res) => {
   res.cookie("auth_token", "", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     expires: new Date(0),
   });
 
