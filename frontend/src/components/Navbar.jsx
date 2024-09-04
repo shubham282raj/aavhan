586ce0362f33dba3755f113aa61c13080774c20c
Shubham Raj <shubham282raj@gmail.com>
2024-09-05 00:06:15 +0530

    Improved navbar styling

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,8 +20,6 @@ export default function Navbar() {
         ["/login", "Login"],
       ];
 
-  const navbarRef = useRef();
-
   const navbarHeight = `sm:min-h-[70px] sm:max-h[70px]`;
 
   return (
@@ -29,21 +27,15 @@ export default function Navbar() {
       <div
         className={navbarHeight}
         style={{
-          height: toggleMenu ? navbarRef.current.scrollHeight + "px" : "60px",
+          height: "60px",
         }}
       ></div>
-      <div className="fixed z-50 top-0 left-0 w-full navbar-bg text-white font-bold">
+      <div className="fixed z-50 top-0 left-0 w-full navbar-bg text-white font-bold sm:font-normal">
         <div
           className={
             "max-w-screen-xl m-auto p-3 flex flex-col sm:flex-row justify-between items-center overflow-hidden transition-[max-height] " +
             navbarHeight
           }
-          ref={navbarRef}
-          style={{
-            maxHeight: toggleMenu
-              ? navbarRef.current.scrollHeight + "px"
-              : "60px",
-          }}
         >
           <div className="flex items-center justify-between w-full sm:w-auto">
             <span className="text-3xl font-bold tracking-tight">
@@ -53,15 +45,50 @@ export default function Navbar() {
               </Link>
             </span>
             <div
-              className="sm:hidden text-3xl cursor-pointer"
+              className="sm:hidden text-3xl cursor-pointer h-7 flex flex-col justify-center items-center w-9 relative"
               onClick={() => setToggleMenu(!toggleMenu)}
             >
-              &#9776;
+              <div
+                className="rounded-full h-[2.5px] bg-white absolute w-full"
+                style={{
+                  transform: toggleMenu
+                    ? "rotate(-45deg)"
+                    : "translate(0, -12px)",
+                  transition: "all 0.3s ease",
+                }}
+              ></div>
+              <div
+                className="rounded-full h-[2.5px] bg-white absolute w-full"
+                style={{
+                  maxWidth: toggleMenu ? "0" : "100%",
+                  transition: "all 0.3s ease",
+                }}
+              ></div>
+              <div
+                className="rounded-full h-[2.5px] bg-white absolute w-full"
+                style={{
+                  transform: toggleMenu
+                    ? "rotate(45deg)"
+                    : "translate(0, 12px)",
+                  transition: "all 0.3s ease",
+                }}
+              ></div>
             </div>
           </div>
-          <div>
+          <div
+            className={
+              "sm:block sm:w-auto sm:h-auto sm:static sm:bg-transparent sm:z-auto absolute -z-10 top-0 w-screen h-screen backdrop-blur-md bg-black bg-opacity-25 transition-all sm:translate-x-0" +
+              (toggleMenu ? " translate-x-[0%]" : " translate-x-[100%]") +
+              " sm:translate-x-0"
+              // (toggleMenu ? " " : " hidden")
+            }
+            style={{
+              transition: "transform 0.3s ease",
+              // transform: toggleMenu ? "translateX(0%)" : "translateX(100%)",
+            }}
+          >
             <div
-              className={`flex flex-col justify-center items-center sm:flex sm:flex-row text-xl gap-5 mt-2`}
+              className={`h-[80%] sm:h-auto flex flex-col justify-center items-center sm:flex sm:flex-row text-xl gap-7 sm:gap-5 mt-2`}
             >
               {navigationLinks.map((link, index) => {
                 return (
